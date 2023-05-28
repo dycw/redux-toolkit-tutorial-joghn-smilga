fef951b0f8795df260c62e0543159e57812a66da
Derek Wan <d.wan@icloud.com>
2023-05-28 20:42:38 +0800

    Refactor & use `dummyjson` (#6)

--- src/slices/cart.tsx
+++ src/slices/cart.tsx
@@ -1,86 +1,53 @@
-import { cartItem } from "../cartItems";
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import axios from "axios";
+import { api } from "../api/api";
+import { Item } from "../items";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const url = "https://course-api.com/react-useReducer-cart-project";
-
-type State = {
-  cartItems: cartItem[];
-  amount: number;
-  total: number;
-  isLoading: boolean;
-};
-const state: State = {
-  cartItems: [],
-  amount: 0,
-  total: 0,
-  isLoading: true,
-};
-
-export const getCartItems = createAsyncThunk(
-  "cart/getCartItems",
-  async (_, thunkAPI) => {
-    try {
-      const resp = await axios(url);
-      return resp.data;
-    } catch (err) {
-      return thunkAPI.rejectWithValue("something went wrong");
-    }
-  }
-);
+type State = { items: Item[] };
+const state: State = { items: [] };
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState: state,
   reducers: {
     clearCart: (state) => {
-      state.cartItems = [];
+      state.items = [];
     },
-    removeItem: (state, action: PayloadAction<string>) => {
-      const itemId = action.payload;
-      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
-    },
-    increase: (state, action: PayloadAction<string>) => {
-      const cartItem = state.cartItems.find(
-        (item) => item.id === action.payload
-      );
-      if (cartItem) {
-        cartItem.amount += 1;
+    removeItem: (state, action: PayloadAction<number>) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item) {
+        item.quantity = 0;
       }
     },
-    decrease: (state, action: PayloadAction<string>) => {
-      const cartItem = state.cartItems.find(
-        (item) => item.id === action.payload
-      );
-      if (cartItem) {
-        cartItem.amount -= 1;
+    increase: (state, action: PayloadAction<number>) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item) {
+        item.quantity += 1;
       }
     },
-    calculateTotals: (state) => {
-      state.amount = state.cartItems.reduce((acc, el) => acc + el.amount, 0);
-      state.total = state.cartItems.reduce(
-        (acc, el) => acc + el.amount * el.price,
-        0
-      );
+    decrease: (state, action: PayloadAction<number>) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item) {
+        item.quantity = Math.max(item.quantity - 1, 0);
+      }
     },
   },
   extraReducers: (builder) => {
-    builder
-      .addCase(getCartItems.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(
-        getCartItems.fulfilled,
-        (state, action: PayloadAction<cartItem[]>) => {
-          state.isLoading = false;
-          state.cartItems = action.payload;
-        }
-      )
-      .addCase(getCartItems.rejected, (state) => {
-        state.isLoading = false;
-      });
+    builder.addMatcher(
+      api.endpoints.getCartItems.matchFulfilled,
+      (state, action) => {
+        state.items = action.payload;
+      }
+    );
   },
 });
 
-export const { clearCart, removeItem, increase, decrease, calculateTotals } =
-  cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+
+export const selectQuantity = (state: State, id: number): number => {
+  const item = state.items.find((i) => i.id === id);
+  return item ? item.quantity : 0;
+};
+export const selectTotalQuantity = (state: State): number =>
+  state.items.reduce((acc, el) => acc + el.quantity, 0);
+export const selectTotalPrice = (state: State): number =>
+  state.items.reduce((acc, el) => acc + el.price * el.quantity, 0);
